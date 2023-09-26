37a4bc46da391cde3c4b94be7748b42866147e0c
adillazaar <adil.lazaar89@gmail.com>
2023-09-26 12:02:46 +0200

    Fin de séance 1

--- src/main/kotlin/jeu/Jeu.kt
+++ src/main/kotlin/jeu/Jeu.kt
@@ -40,12 +40,58 @@ class Jeu(monstres: List<Personnage>) {
      *  Méthode pour créer le personnage du joueur en demandant les informations à l'utilisateur
      *
      */
+
+
     fun creerPersonnage(): Personnage {
-        println("Création votre personnage:")
-        // TODO Mission 1.1
-        val hero = Personnage("YYY",150,150,12,8,8,12)
-        this.joueur= hero
-        return hero
+        println("Création de votre personnage:")
+
+        // Initialiser les attribues
+        var nom: String = ""
+        var attaque: Int = 0
+        var defense: Int = 0
+        var endurance: Int = 0
+        var vitesse: Int = 0
+
+        // demander le nom du joueur
+        println("Entrez le nom du personnage:")
+        nom = readLine() ?: ""
+
+        // repartir l'ensemble des 40 points
+        var restantPoints = 40
+        while (restantPoints > 0) {
+            println("Points restants: $restantPoints")
+
+            print("Points d'attaque (0-$restantPoints): ")
+            attaque = readLine()?.toIntOrNull() ?: 0
+            restantPoints = restantPoints - attaque
+            print("Points de défense (0-$restantPoints): ")
+            defense = readLine()?.toIntOrNull() ?: 0
+            restantPoints = restantPoints - defense
+            print("Points d'endurance (0-$restantPoints): ")
+            endurance = readLine()?.toIntOrNull() ?: 0
+            restantPoints = restantPoints - endurance
+            print("Points de vitesse (0-$restantPoints): ")
+            vitesse = readLine()?.toIntOrNull() ?: 0
+            restantPoints = restantPoints - vitesse
+            val totalPoints = attaque + defense + endurance + vitesse
+
+            if (restantPoints >= 0) {
+                break
+            } else {
+                println("Le total des points d'attributs ne doit pas dépasser 40.")
+            }
+        }
+
+        // calcul de la vie selon l'endurance
+        val maxvie = 50 + endurance * 10
+
+        // creation des atribue
+        val joueuratribut = Personnage(nom, maxvie, maxvie, attaque, defense, endurance, vitesse)
+
+        // Assigner la creation du personnage au attribue du joueur character to the joueur attribute
+        joueur = joueuratribut
+
+        return joueuratribut
     }
     
 
